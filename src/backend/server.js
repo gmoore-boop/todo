dcd95fe6e8206c4a719f597e5235b406f10ec264
Moura Guilherme <amorimdemouraguilherme@gmail.com>
2026-03-10 10:21:43 -0300

    chore: reorganize project structure and initialize server

--- src/backend/server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-
-  console.log(req.__proto__);
-  res.end("Check terminal");
-})
-server.listen(3001, () => {
-  console.log("Server running on port 3000");
-});
